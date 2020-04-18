1248fa66d41333fe56f9a07c6898075fc54938ff
James Usevitch <usevitch@umich.edu>
2020-04-18 15:09:29 -0400

    IO trajectory tracking works for one quadrotor
    The node IO_controller_quad works for one quadrotor. The quadrotor
    tracks a circular trajectory in the air. The launch file
    "one_IO_quad.launch" runs the simple test.
    
    Also changed the commenting at the beginning of some of the
    documents to be actual Python comments instead of multi-
    line strings (since these will be interpreted as docstrings)
    
    Some commenting may need to be removed from IO_controller_quad.

--- src/trajectory.py
+++ src/trajectory.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
-# File containing functions which calculate the position of a point along
-# a time-varying trajectory. 
+# 
+#      ____  ___   _____ ______   __          __    
+#     / __ \/   | / ___// ____/  / /   ____ _/ /_   
+#    / / / / /| | \__ \/ /      / /   / __ `/ __ \  
+#   / /_/ / ___ |___/ / /___   / /___/ /_/ / /_/ /  
+#  /_____/_/  |_/____/\____/  /_____/\__,_/_.___/   
+#                                                   
+# 
+# Author: James Usevitch
+# 
+# Description: File containing functions which calculate the position of a point along
+#              a time-varying trajectory. 
+# 
+
 
 import numpy as np 
 
@@ -25,6 +37,26 @@ def circular2D(t, c=(0,0), R=1, w=0.1, theta0=0):
     point_deriv = np.array([-R*w*np.sin(w*t + theta0), R*w*np.cos(w*t + theta0)])
     return (point, point_deriv)
 
+
+def circular3D(t, c=(0,0,0), R=1, w=0.1, theta0=0):
+    # TODO: Currently this just specifies a circle parallel to the floor. Make option to
+    #       rotate the circle arbitrarily
+    # Parameters:
+    #   t : Time variable. Assumes t0 = 0.
+    #   c : Array-like object with x,y,z coordinates of circle center. Can be
+    #       numpy array or anything you can make a numpy array out of.
+    #   R : Radius of circle
+    #   theta0 : Initial offset angle of line between point and c and the
+    #            x-axis of world frame. For example, theta0 = 0 will start
+    #            the point moving from the position [R+c[0], 0]. 
+    #   w : Angular velocity. Determines how fast point moves around
+    #       edge of circle.
+
+    # Returns both the point and the time derivative of the point (i.e. velocity)
+    
+    point = np.array([c[0] + R*np.cos(w*t + theta0), c[1] + R*np.sin(w*t + theta0), c[2]])
+    point_deriv = np.array([-R*w*np.sin(w*t + theta0), R*w*np.cos(w*t + theta0), 0])
+    return (point, point_deriv)
 
 # def square:
 
