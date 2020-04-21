15e409b69bd18ec39ba018dcd5853da3e256c06b
James Usevitch <usevitch@umich.edu>
2020-04-20 20:32:51 -0400

    Began msrpa.py

---
+++ src/msrpa.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+# 
+#      ____  ___   _____ ______   __          __    
+#     / __ \/   | / ___// ____/  / /   ____ _/ /_   
+#    / / / / /| | \__ \/ /      / /   / __ `/ __ \  
+#   / /_/ / ___ |___/ / /___   / /___/ /_/ / /_/ /  
+#  /_____/_/  |_/____/\____/  /_____/\__,_/_.___/   
+#                                                   
+# 
+# author: james usevitch
+# 
+# Description: This file contains the input/output linearization controller for the
+#              Hector Quadrotor model in Gazebo. Obstacle avoidance and input constraint satisfaction
+#              is calculated using a control barrier function and QP method.
+# 
+
+
+import rospy
+import sys
+import numpy as np
+import argparse
+from copy import deepcopy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    
+
